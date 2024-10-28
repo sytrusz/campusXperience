0b2a19c0fc6d9d86c8bbc0de2d0db29a0e25f10c
Nathan Rener Malagapo <nathanrener.malagapo@cit.edu>
2024-10-28 13:45:54 +0800

    added-event-controller/service

--- backend/src/main/java/com/wachichaw/backend/entity/EventEntity.java
+++ backend/src/main/java/com/wachichaw/backend/entity/EventEntity.java
@@ -56,6 +56,10 @@ public class EventEntity {
         this.maxCapacity = maxCapacity;
     }
 
+    public EventEntity() {
+        //TODO Auto-generated constructor stub
+    }
+
     public int getEventId(){
         return eventId;
     }
@@ -66,6 +70,14 @@ public class EventEntity {
 
     public String getTitle(){
         return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getDescription(){
+        return description;
     }
 
     public void setDescription(String description){
