e25bbba8cc7a547431594a4907f1f268166dfb65
piolonrqz <piolofrances.enriquez@cit.edu>
2024-11-02 23:10:43 +0800

    added react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,27 @@
-import './App.css'
-import Appbar from './components/Appbar'
-import Signup from './components/Signup'
-import Login from './components/Login'
-import ReminderApp from './components/Reminder'
+import './App.css';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReminderApp from './components/Reminder';
+import { Login } from '@mui/icons-material';
+import Signup from './components/Signup';
 function App() {
-
   return (
     <>
       {/* <Appbar/>
       <Signup/>
       <Login/> */}
-      <ReminderApp/>
+      <ReminderApp />
       <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Homepage />} />
-                <Route path="/admin-dashboard" element={<AdminDashboard />} />
-                <Route path="/user-dashboard" element={<UserDashboard />} />
-                <Route path="/signup" element={<SignupPage/>} />
-                <Route path='/login' element={<LoginPage/>} />
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Homepage />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/user-dashboard" element={<UserDashboard />} /> */}
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
