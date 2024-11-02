db15632303f38e472fd5cc8afe2692d19d659c04
piolonrqz <piolofrances.enriquez@cit.edu>
2024-11-02 23:15:15 +0800

    fix reminder

--- backend/src/main/java/com/wachichaw/backend/controller/ReminderController.java
+++ backend/src/main/java/com/wachichaw/backend/controller/ReminderController.java
@@ -1,47 +1,49 @@
 package com.wachichaw.backend.controller;
-
+ 
 import java.util.List;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+ 
 import com.wachichaw.backend.entity.ReminderEntity;
 import com.wachichaw.backend.service.ReminderService;
-
+ 
 @RestController
 @RequestMapping("/reminder")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ReminderController {
-
+ 
     @Autowired
     private ReminderService reminderService;
-
+ 
     // Check
     @GetMapping("/print")
     public String print() {
         return "Hello, Reminder! Test";
     }
-
+ 
     // Create
     @PostMapping("/save")
     public ReminderEntity saveReminder(@RequestBody ReminderEntity reminder) {
         return reminderService.saveReminder(reminder);
     }
-
+ 
     // Read
     @GetMapping("/getAll")
-    public List<ReminderEntity> getAllReminder() {
-        return reminderService.getAllReminder();
+    public List<ReminderEntity> getAllReminders() {
+        return reminderService.getAllReminders();
     }
-
+ 
     // Update by ID
     @PutMapping("/update")
     public ReminderEntity updateReminder(@RequestParam int reminderId, @RequestBody ReminderEntity reminder) {
         return reminderService.updateReminder(reminderId, reminder);
     }
-
+ 
     // Delete by ID
     @DeleteMapping("/delete/{reminderId}")
     public String deleteReminder(@PathVariable int reminderId){
         return reminderService.deleteReminder(reminderId);
     }
 }
+ 
