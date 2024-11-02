f73ab7f01ffc3ec9b187e8975f1ecc53f748d58b
Nathan Rener Malagapo <nathanrener.malagapo@cit.edu>
2024-11-02 14:12:59 +0800

    fix-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,24 @@ import Appbar from './components/Appbar'
 import Signup from './components/Signup'
 import Login from './components/Login'
 import ReminderApp from './components/Reminder'
+import UserRecord from './components/UserRecord'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
 
   return (
     <>
-      {/* <Appbar/>
-      <Signup/>
-      <Login/> */}
+      <Appbar/>
+
       <ReminderApp/>
+      <UserRecord/>
       <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Homepage />} />
-                <Route path="/admin-dashboard" element={<AdminDashboard />} />
-                <Route path="/user-dashboard" element={<UserDashboard />} />
-                <Route path="/signup" element={<SignupPage/>} />
-                <Route path='/login' element={<LoginPage/>} />
+                {/* <Route path="/" element={<Homepage />} /> */}
+                {/* <Route path="/admin-dashboard" element={<AdminDashboard />} />
+                <Route path="/user-dashboard" element={<UserDashboard />} /> */}
+                <Route path="/signup" element={<Signup/>} />
+                <Route path='/login' element={<Login/>} />
             </Routes>
         </BrowserRouter>
     </>
