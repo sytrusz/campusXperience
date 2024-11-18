0d85a50180e626f593b216997e52f20c9dab1621
jerjenres <jerjenres.pangalay@cit.edu>
2024-11-18 15:43:12 +0800

    addmin

--- backend/src/main/java/com/wachichaw/backend/controller/UserController.java
+++ backend/src/main/java/com/wachichaw/backend/controller/UserController.java
@@ -15,7 +15,7 @@ import com.wachichaw.backend.service.UserService;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
     @Autowired
